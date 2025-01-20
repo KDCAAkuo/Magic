6a7d91330f1da809deb8548675f5f5444ada778a
KDCAAkuo <m2216012ia@ug.jwu.ac.jp>
2025-01-20 16:03:16 +0900

    Update ai.py

--- ai.py
+++ ai.py
@@ -129,7 +129,7 @@ def minimax(board, stone, depth, maximizing_player, alpha, beta):
 
 class MagicAI(object):
     def face(self):
-        return "🪄"
+        return "⛄"
 
     def place(self, board, stone):
         # Dynamic depth adjustment
